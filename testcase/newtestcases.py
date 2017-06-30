6391ec4c524eba029da1005bcb8b9abe66558bef
KingOfNight <chenfjhaoshuai@hotmail.com>
2017-06-30 17:25:35 +0800

    add new TestCase
    add new TestCase

---
+++ testcase/newtestcases.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+class test:
+    def __init__(self):
+        return
+    def importData(self):
+        return
+    def cleanData(self):
+        return
+
+class testcases:
+    def check_price(self):
+        return
+    def check_items(self):
+        return
+    def check_saleoff(self):
+        return
+    def check_memberoff(self):
+        return
