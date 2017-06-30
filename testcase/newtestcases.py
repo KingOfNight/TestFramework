74a826997f5fc89e757ffb481cb1d0d13e22bfe5
KingOfNight <chenfjhaoshuai@hotmail.com>
2017-06-30 18:24:25 +0800

    update

--- testcase/newtestcases.py
+++ testcase/newtestcases.py
@@ -10,6 +10,12 @@ class test:
         return
 
 class testcases:
+    def testpot(self):
+        s=[{'shopid':12,'fromw':2}]
+        j=json.dump(s)
+        # shopid int 店铺id; openid string openid; fromw int 1app 2微信 3支付宝
+        r=requests.post(" http://qr-st.api.9now.net/",j)
+        return
     def check_price(self):
         return
     def check_items(self):
