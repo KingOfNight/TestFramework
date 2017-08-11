a3816f81bfce723470454af19d8b6c322fd4a0d8
KingOfNight <chenfjhaoshuai@hotmail.com>
2017-08-11 19:05:44 +0800

    1

--- testcase/testcases.py
+++ testcase/testcases.py
@@ -1,19 +1,19 @@
 import requests
 import json
+import types
 
-class test:
-    def __init__(self):
-        return
-    def importData(self):
-        return
-    def cleanData(self):
-        return
+class shop():
+    shopId=1    #店铺id
+    shopName='shopname'  #店铺名
+    managerShopId=43     #总店id
+    openid = 'oWT18jmvvJ50Q-GhEPUeY0cgAp4U'
+    def shop(self):
 
-class testcases(test):
-    def testpot(self):
-        s=[{'shopid':12,'fromw':2}]
-        # shopid int 店铺id; openid string openid; fromw int 1app 2微信 3支付宝
         return
+    def set_shop_name(self,shopName):
+        shopName=
+        return
+class testcases():
     def check_price(self):
         return
     def check_items(self):
@@ -23,12 +23,65 @@ class testcases(test):
     def check_memberoff(self):
         return
 
-
-def fc():
-    url="http://qr-api.uat.9now.net/pay/api/shop.detail"
-    r=requests.post(url,)
-    j={"MemID":"028715840463","PayType":0,"mworderId":"3067158","orderId":"86548","paymentList":[{"couponDiscount":"0","paymentMoney":"0","paymentType":"6","pointPayment":"5","presentCash":"0"}],"phoneNum":"15800514409","status":"pay","tableName":"101","tableNo":"101","total":"5"}
-    json_str=json.dumps(j)
-
-    print()
+def get_dict_value(keyName,dictName):
+    for key,val in dictName.items():
+        print(key)
+        if (key ==keyName):
+            print(val)
+            return val
+        else:
+            if(isinstance(val,dict)):
+                get_dict_value(keyName,val)
     return
+
+def compare_dict(dict1,dict2):
+
+    return
+
+
+def get_shop():
+    shopUrl="http://qr-api.uat.9now.net/pay/api/shop.detail"
+    #shopId=140375&tableId=101&tableNo=101&codeType=1&openid=oWT18jmvvJ50Q-GhEPUeY0cgAp4U&userid=39218614&fromw=2
+    #shopData={'shopId':'140375','tableId':'101','tableNo':'101','codeType':'1','openid':'oWT18jmvvJ50Q-GhEPUeY0cgAp4U','userid':'39218614','fromw':'2'}
+    shopData={'shopId':'140375','openid':'oWT18jmvvJ50Q-GhEPUeY0cgAp4U','fromw':'2'}
+    headers={'Accept':'application/json'}
+    try:
+        shopInform=requests.post(shopUrl,data=shopData,headers=headers)
+        shop=shopInform.json()
+        isinstance(shop,dict)
+    except BaseException:
+        print("出现异常")
+    else:
+        print(shopInform.json())
+    return -1
+
+def get_order():
+    shopId='140375'
+    managerShopId='43'
+    shopName='美味不用等(正大广场店)'
+    thirdAccess='3'
+    openid='oWT18jmvvJ50Q-GhEPUeY0cgAp4U'
+    #code='6ie713nrt5e939'
+    orderUrl="http://qr-api.uat.9now.net/pay/api/order.detail"
+    headers = {'Accept': 'application/json'}
+    orderData ={'shopId':shopId,'tableId':'001','tableNo':'001','managerShopId':managerShopId,'shopName':shopName,'thirdAccess':thirdAccess,'openid':openid}
+    try:
+        orderInform=requests.post(orderUrl,data=orderData,headers=headers)
+    except BaseException:
+        print("errors")
+    else:
+        print(orderInform.json())
+    return orderInform.json()
+
+def main():
+    keyName='discountAmountPay'
+    try:
+        orderDict=get_order()
+        discountAmountPayValue=get_dict_value(keyName,orderDict)
+    except BaseException:
+        print("error")
+    else:
+        print(discountAmountPayValue)
+str=get_order()
+
+get_dict_value('discountAmountPay',str)
